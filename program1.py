eb49d42fb42478647fe7d716c37610a1152b4534
chirag yadav <107507592+chiragyadav1711@users.noreply.github.com>
2024-10-08 15:34:53 +0530

    Update program1.py
    solution Que1

--- program1.py
+++ program1.py
@@ -4,6 +4,30 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+
+        matching_bracket = {')' : '(' , '}' : '{' , ']' : ' ['}
+
+        stack = []
+
+        for char in s
+
+        if char in matching_bracket:
+            top_element = stack.pop() if stack else '#'
+
+        if matching_bracket[char] != top_element:
+            return false
+
+        else:
+            stack.append(char)
+
+        return not stack
+
+        solution = Solution()
+
+        print(solution.isValid("()"))
+        print(solution.isValid("() [] {}"))
+        print(solution.isValid("(]"))
+        
         pass
 
 
