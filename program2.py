092f7f932885138fe90a296a2331412742fe488e
chirag yadav <107507592+chiragyadav1711@users.noreply.github.com>
2024-10-08 15:44:18 +0530

    Update program2.py
    solution Que2

--- program2.py
+++ program2.py
@@ -4,7 +4,29 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+
+        roman_map{'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000} 
+
+        total = 0;
+        prev_value = 0;
+
+        for char in s:
+            curr_value = roman_map[char]
+
+        if curr > prev_value:
+            total += curr_value - 2* prev_value
+        else:
+            total += curr_value
+
+        prev_value = curr_value
+
+        return total
+
+        solution = Solution()
+        print(solution.romanToInt("III"))
+        print(solution.romanToInt("LVIII"))
+        print(solution.romanToInt("MCMXCIV"))
+        
 
 
 
